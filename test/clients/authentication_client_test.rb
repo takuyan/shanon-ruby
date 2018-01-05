9832a27cb5cb0688009016dc5a9b681912e2a281
Takuya Kato <github@takuyan.com>
2018-01-06 01:14:07 +0900

    move stub_request to parent class
    every client need token, then every client call authentication.get

--- test/clients/authentication_client_test.rb
+++ test/clients/authentication_client_test.rb
@@ -3,18 +3,9 @@ require 'securerandom'
 
 class AuthenticationClientTest < BaseClientTest
   setup do
-    @client = Shanon::Clients::AuthenticationClient.new
+    stub_authentication
 
-    @dummy_token = SecureRandom.hex(10)
-    response_body = %(
-      <?xml version="1.0" encoding="UTF-8"?>
-      <Response xmlns="http://smartseminar.jp/" version="1.53">
-        <Token>#{@dummy_token}</Token>
-      </Response>
-    )
-    stub_request(:get, 'https://aaaa-bbbb.smktg.jp/services/rest/authentication?api_key=aaaa&api_sig=d639fb4bda55afd8101a2708cb1303a9de490c87&password=bbbb')
-      .with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Faraday v0.13.1'})
-      .to_return(status: 200, body: response_body, headers: {})
+    @client = Shanon::Clients::AuthenticationClient.new
   end
 
   test 'should return token' do
