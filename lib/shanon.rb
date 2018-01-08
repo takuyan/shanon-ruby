b4ef211c062a7f989464d3302033fafd8b66f42c
Takuya Kato <github@takuyan.com>
2018-01-08 21:18:00 +0900

    add EnqueteClient
    see: http://apidoc.shanon.co.jp/ja/api/visitor/enquete/get.html

--- lib/shanon.rb
+++ lib/shanon.rb
@@ -5,6 +5,7 @@ require 'shanon/abstract_client'
 require 'shanon/authenticatable'
 require 'shanon/clients/authentication_client'
 require 'shanon/clients/visitor_client'
+require 'shanon/clients/enquete_client'
 
 module Shanon
   # Your code goes here...
